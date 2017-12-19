6a42dff214df9f0411db88036d790f00fb67fb8f
Marty McGuire <schmartissimo@gmail.com>
2017-12-19 14:19:05 -0500

    No BBC watermark, plz

--- audiogram/initialize-canvas.js
+++ audiogram/initialize-canvas.js
@@ -10,11 +10,6 @@ function initializeCanvas(theme, cb) {
   // Fonts pre-registered in bin/worker
   var renderer = getRenderer(theme);
 
-  // Load BBC watermark
-  var dog = new Canvas.Image;
-  dog.src = path.join(serverSettings.storagePath, "../editor/images/bbc.png");
-  renderer.bbcDog(dog);
-
   // Load foreground image
   if (theme.customForegroundPath || theme.foregroundImage) {
     var fg = new Canvas.Image;
